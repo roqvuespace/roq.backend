44224d520c44537ee6756353f43010d84de565e0
ternicinc <allexander.linux@gmail.com>
2025-08-24 15:32:30 +0200

    Route name changed to /check, so we can easily orginize.

--- src/app.api/api.routes/api.v1/v1.ts
+++ src/app.api/api.routes/api.v1/v1.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => {
+router.get('/check', (req, res) => {
     res.json({ message: 'API Server V1 is running, welcome.' })
 });
 
